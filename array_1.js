1deadb917e1830764791678ade9a3664f7f30ec7
Hoangthaikha <55891419+Hoangthaikha@users.noreply.github.com>
2024-02-25 01:35:18 +0700

    Create array_1.js

---
+++ array_1.js
@@ -0,0 +1,46 @@
+var person1 = {
+  name: "kha",
+  age: 25,
+  gender: "male",
+};
+
+var person2 = {
+  name: "hà",
+  age: 21,
+  gender: "female",
+};
+
+var person3 = {
+  name: "tú",
+  age: 22,
+  gender: "female",
+};
+
+var person4 = {
+  name: "tài",
+  age: 25,
+  gender: "male",
+};
+
+var person5 = {
+  name: "tuấn",
+  age: 21,
+  gender: "male",
+};
+
+var arrayPerson = [person1, person2,person3,person4,person5];
+
+console.log(arrayPerson);
+
+// update vào list person6 vào person5
+
+var person6 = {
+  name: "Nhân",
+  age: 37,
+  gender: "male",
+};
+
+// thêm person6 vào danh sách
+arrayPerson.push[person6];
+console.log(arrayPerson);
+
